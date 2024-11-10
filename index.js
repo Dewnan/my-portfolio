547405410ea943c0823918e904dd7566a60e6467
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-11-10 20:30:31 +0530

    add responsive css

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var tab_links = document.getElementsByClassName("tab-links")
-var tab_contents = document.getElementsByClassName("tab-contents")
+var tab_links = document.getElementsByClassName("tab-links");
+var tab_contents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabName) {
     for (tab_link of tab_links) {
-        tab_link.classList.remove("active-link")
+        tab_link.classList.remove("active-link");
     }
     for (tab_contect of tab_contents) {
-        tab_contect.classList.remove("active-tab")
+        tab_contect.classList.remove("active-tab");
     }
 
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabName).classList.add("active-tab")
+}
+
+var sideMenue = document.getElementById("sideMenue");
+
+function openMenue(){
+    sideMenue.style.right = "0"
+}
+function closeMenue(){
+    sideMenue.style.right = "-215px"
 }
